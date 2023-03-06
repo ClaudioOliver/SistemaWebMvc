ab3a1f0bc779930ea9d40b2afa7637c433ecf419
Claudio Oliveira <claudio.oliveira127@outlook.com>
2023-03-06 15:07:46 -0300

    First Model-Controller-View - Department

--- SistemaWebMvc/Controllers/HomeController.cs
+++ SistemaWebMvc/Controllers/HomeController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SistemaWebMvc.Models;
+using SistemaWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace SistemaWebMvc.Controllers
 {
